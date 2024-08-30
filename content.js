c75c1925a37f126a5026f3503a643263ce6a16d5
hyunyikim <hailey@mass-adoption.com>
2024-08-30 22:50:02 +0200

    feat: add title to nav

--- content.js
+++ content.js
@@ -1,19 +1,40 @@
-console.log("hello world! it's content for TitleViewer");
-
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('loaded!!!!');
-
   let isContentLoaded = false;
   const titleElements = document.getElementsByTagName('title');
 
   const intervalId = setInterval(() => {
     if (titleElements?.length > 0 && !isContentLoaded) {
-      const title = (titleElements[0].textContent || '').trim();
+      const titleElement = titleElements[0];
+      const title = (titleElement.textContent || '').trim();
       console.log('title', title);
 
       if (title !== 'ChatGPT') {
         isContentLoaded = true;
         clearInterval(intervalId);
+
+        // Add title to Nav
+        const articles = document.querySelectorAll('article');
+
+        // TODO: if there is a chat before starting?
+        if (articles?.length > 0) {
+          const navElement = articles[0]?.previousElementSibling;
+          console.log('navElement', navElement);
+          if (navElement && navElement?.tagName === 'DIV') {
+            console.log('navElement is DIV');
+
+            const newTitleElement = document.createElement('div');
+            newTitleElement.textContent = title;
+            // TODO: styling
+            // TODO: text ellipsis
+            newTitleElement.setAttribute('class', `flex items-center gap-2 pr-1 leading-[0]`);
+            // newTitleElement.style.fontWeight = 'bold'; // Optional styling
+
+            const lastNavChildElement = navElement.lastChild;
+            if (lastNavChildElement) {
+              navElement.insertBefore(newTitleElement, lastNavChildElement);
+            }
+          }
+        }
       }
     }
   }, 500);
