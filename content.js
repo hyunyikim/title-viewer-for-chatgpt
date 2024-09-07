fa7ad7c04382989a0ecc812f50a7f2d19f6805cd
hyunyikim <hailey@mass-adoption.com>
2024-09-07 17:45:24 +0200

    feat: add detect title changes feature

--- content.js
+++ content.js
@@ -1,38 +1,50 @@
 // TODO: when click on other chat
 
 const INITIAL_CHAT_NAME = 'ChatGPT';
+const DETECT_ARTICLE_MAX_COUNT = 3;
 
 document.addEventListener('DOMContentLoaded', function () {
   const onAddTitleToGnb = (title) => {
-    const articles = document.querySelectorAll('article');
-    if (articles?.length > 0) {
-      const gnbElement = articles[0]?.previousElementSibling;
-      console.log('gnbElement', title);
+    let count = 0;
 
-      if (gnbElement && gnbElement?.tagName === 'DIV') {
-        console.log('gnbElement is DIV');
+    // detect article elements
+    const addTitleToGnbIntervalId = setInterval(() => {
+      count += 1;
+      const articles = document.querySelectorAll('article');
 
-        // Already existed title
-        const existedTitleElement = document.querySelector('.chatgpt-chat-title');
-        if (existedTitleElement) {
-          existedTitleElement.textContent = title;
-          return;
-        }
+      // try limited times
+      if (count > DETECT_ARTICLE_MAX_COUNT) {
+        clearInterval(addTitleToGnbIntervalId);
+        return;
+      }
 
-        // No title
-        const newTitleElement = document.createElement('div');
-        newTitleElement.textContent = title;
+      if (articles?.length > 0) {
+        clearInterval(addTitleToGnbIntervalId);
 
-        newTitleElement.setAttribute(
-          'class',
-          `block text-ellipsis overflow-hidden whitespace-nowrap flex-1 overflow-hidden items-center gap-2 px-1 text-token-text-secondary chatgpt-chat-title`
-        );
-        const lastNavChildElement = gnbElement.lastChild;
-        if (lastNavChildElement) {
-          gnbElement.insertBefore(newTitleElement, lastNavChildElement);
+        const gnbElement = articles[0]?.previousElementSibling;
+        if (gnbElement && gnbElement?.tagName === 'DIV') {
+          // Already existed title
+          const existedTitleElement = document.querySelector('.chatgpt-chat-title');
+          if (existedTitleElement) {
+            existedTitleElement.textContent = title;
+            return;
+          }
+
+          // No title
+          const newTitleElement = document.createElement('div');
+          newTitleElement.textContent = title;
+
+          newTitleElement.setAttribute(
+            'class',
+            `block text-ellipsis overflow-hidden whitespace-nowrap flex-1 overflow-hidden items-center gap-2 px-1 text-token-text-secondary chatgpt-chat-title`
+          );
+          const lastNavChildElement = gnbElement.lastChild;
+          if (lastNavChildElement) {
+            gnbElement.insertBefore(newTitleElement, lastNavChildElement);
+          }
         }
       }
-    }
+    }, 500);
   };
 
   // after content initially loaded
@@ -47,4 +59,27 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     }
   }, 500);
+
+  // detect title changes
+  const detectNavChangeIntervalId = setInterval(() => {
+    const navElement = document.querySelector('nav');
+
+    if (navElement) {
+      const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+          const selectedChatTitle = document.querySelector('nav li .bg-token-sidebar-surface-secondary a > div')?.textContent;
+
+          if (selectedChatTitle) {
+            onAddTitleToGnb(selectedChatTitle);
+          }
+        });
+      });
+      observer.observe(navElement, { subtree: true, attributeFilter: ['class'] });
+
+      clearInterval(detectNavChangeIntervalId);
+    }
+  }, 500);
+
+  // TODO:
+  //   // observer.disconnect();  // Use this to stop observing if needed
 });
